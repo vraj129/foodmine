1b7bb4e65a0d704707043d766aaadd192bfb516f
vraj129 <vrajpatelv@protonmail.com>
2023-06-07 22:14:08 +0530

    tags component and food page

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -16,9 +16,10 @@ export class HomeComponent implements OnInit {
       if(params.searchTerm){
         this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
       }
+      else if(params.tag) {
+        this.foods = this.foodService.getAllFoodsByTag(params.tag);
+      }
       else {
-
-
         this.foods = foodService.getAll();
       }
     });
