59e54c3f80929ec864ad4fb4d4514a0adcbafb91
vraj129 <vrajpatelv@protonmail.com>
2023-06-08 12:18:35 +0530

    components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
     SearchComponent,
     TagsComponent,
     FoodPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItems } from 'src/app/shared/models/CartItems';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent {
+  cart!:Cart;
+  constructor(private cartService:CartService){
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
 
+  removeFromCart(cartItem:CartItems) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+  changeQuantity(cartItem:CartItems,quantityInString:string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id,quantity);
+  }
 }
