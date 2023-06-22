ea5e3eca188c20fe0d7da6eaa3315b8c7bb9b781
vraj129 <vrajpatelv@protonmail.com>
2023-06-22 05:41:34 +0530

    [FEATURE] -- map functionality and adding auth gurad

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
-import foodRouter from './routers/food.router'; 
+import foodRouter from './routers/food.router';
 import userRouter from './routers/user.router';
 import { dbConnect } from './configs/database.config';
 dbConnect();
@@ -13,14 +13,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    credentials:true,
-    origin:["http://localhost:4200"]
+    credentials: true,
+    origin: ["http://localhost:4200"]
 }));
 
-app.use("/api/foods",foodRouter);
-app.use("/api/user",userRouter);
+app.use("/api/foods", foodRouter);
+app.use("/api/user", userRouter);
 
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
