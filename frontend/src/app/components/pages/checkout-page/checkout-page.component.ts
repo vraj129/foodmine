1f138b423855db82cc6457871b51b0d1993cba2a
vraj129 <vrajpatelv@protonmail.com>
2023-06-22 17:37:38 +0530

    [Feature] Checkout page

--- frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CartService } from 'src/app/services/cart.service';
+import { OrderService } from 'src/app/services/order.service';
 import { UserService } from 'src/app/services/user.service';
 import { Order } from 'src/app/shared/models/Order';
 
@@ -13,7 +15,8 @@ import { Order } from 'src/app/shared/models/Order';
 export class CheckoutPageComponent implements OnInit {
   order:Order = new Order();
   checkOutForm!: FormGroup;
-  constructor(cartService: CartService, private formBuilder: FormBuilder, private userService : UserService, private toastrService:ToastrService) {
+  constructor(cartService: CartService, private formBuilder: FormBuilder, private userService : UserService, 
+    private toastrService:ToastrService, private orderService:OrderService, private router:Router) {
 
     const cart = cartService.getCart();
     this.order.items = cart.items;
@@ -37,10 +40,23 @@ export class CheckoutPageComponent implements OnInit {
       this.toastrService.warning('Please fill the inputs', 'Invalid Inputs');
       return;
     }
+    if(!this.order.addressLatLng) {
+      this.toastrService.error('Please select your location on the map', 'Location');
+      return;
+    }
+
 
     this.order.name = this.fc.name.value;
     this.order.address = this.fc.address.value;
 
-    console.log(this.order);
+    this.orderService.create(this.order).subscribe({
+      next:() => {
+        this.router.navigateByUrl('/payment');
+      },
+      error: (errorResponse) => {
+        this.toastrService.error(errorResponse.error,'Cart');
+      }
+    });
+    
   }
 }
